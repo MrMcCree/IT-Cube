438c6a7ea39689937dec9dc833bef22ea67b0934
MrMcCree <157309277+MrMcCree@users.noreply.github.com>
2024-01-22 02:33:44 +0800

    Add files via upload

---
+++ Урок 2. Ввод и вывод данных/task6.py
@@ -0,0 +1,14 @@
+# Задание 6
+
+#one=input("Введите первое слово: ")
+#two=input('Введите второе слово: ')
+#three=input('Введите третье слово: ')
+#print(one, two, three, sep="\n")
+#print(three, two, one, sep="\n")
+
+one=input("Введите первое слово: ")
+two=input('Введите второе слово: ')
+three=input('Введите третье слово: ')
+print(one, two, three, sep="\n")
+print(three, two, one, sep="\n")
+
